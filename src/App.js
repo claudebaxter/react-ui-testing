cf5a83015c01d324dbba2968685f6943c5f501ee
elborracho420 <e.baxter@dark-coin.io>
2023-03-15 17:31:35 -0500

    Setting up UI for a leaderboard that will fetch and sort data to rank players by scores.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
 
+const players = [
+  {
+    characterImg: 'https://ipfs.io/ipfs/Qmdy9r3M1EkhBNahzeHZmeLDtVRHTuK3k6hi5FFJiPEjuv',
+    characterName: 'Meatball Ron',
+    wins: 13,
+    losses: 4,
+    earnings: 100000,
+    costs: 10000
+  },
+  {
+    characterImg: 'https://ipfs.io/ipfs/QmbM1c4vbBFAG5dpsP21Eawcjm51ZXM3FiKEULVJsUNtuz',
+    characterName: 'Skully',
+    wins: 18,
+    losses: 6,
+    earnings: 130000,
+    costs: 10000
+  },
+  {
+    characterImg: 'https://ipfs.io/ipfs/QmXmYHhjM4aVxpjC37UF5oy1C9STXthu8g6AsT4rZX7iTD',
+    characterName: 'Odin',
+    wins: 15,
+    losses: 6,
+    earnings: 105000,
+    costs: 15000
+  },
+  {
+    characterImg: 'https://ipfs.io/ipfs/QmejW5UYbMMtV64y9faC9bwqhNSvGxULbeBMGuoeZcjMgj',
+    characterName: 'Evil Pooh',
+    wins: 9,
+    losses: 3,
+    earnings: 75000,
+    costs: 15000
+  }
+];
+
+
 function App() {
+  // sort the players by wins, then by losses, then by earnings
+  const sortedPlayers = players.sort((a, b) => {
+    if (a.wins !== b.wins) {
+      return b.wins - a.wins; // sort by wins descending
+    } else if (a.losses !== b.losses) {
+      return a.losses - b.losses; // sort by losses ascending
+    } else {
+      return b.earnings - a.earnings; // sort by earnings descending
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+    <h1 style={{
+        display: 'flex',
+        margin: 'auto',
+        justifyContent: 'center',
+        color: 'white'
+    }}>
+        Dark Coin Arena Leaderboard
+    </h1>
+    <ol
+      style={{
+        backgroundColor: 'black',
+        color: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        border: '5px solid white',
+        borderRadius: '15px',
+        marginLeft: '15%',
+        marginRight: '15%',
+        marginTop: '5%',
+        padding: '20px',
+        flexDirection: 'column',
+        paddingLeft: '20px'
+      }}
+    >
+      <h1 style={{
+        display: 'flex',
+        margin: 'auto',
+        justifyContent: 'center'
+    }}>
+        BATTLE RANK
+      </h1>
+
+      {sortedPlayers.map((player, index) => (
+        <li 
+          key={index}
+          style={{
+            border: index % 2 === 0 ? '2px solid white' : 'none',
+            borderRadius: '15px',
+            marginBottom: '10px',
+            padding: '20px',
+            display: 'flex',
+            alignItems: 'center'
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div style={{ marginRight: '20px' }}>{index + 1}{index === 0 ? 'st' : index === 1 ? 'nd' : index === 2 ? 'rd' : 'th'}</div> {/* display rank */}
+          <img src={player.characterImg} alt="character" style={{ height: '200px', flex: '0 0 200px', marginRight: '20px' }} /> {/* display character image */}
+          <div style={{ flex: '1' }}>
+            <div>{player.characterName}</div> {/* display character name */}
+            <div>Wins: {player.wins}</div> {/* display wins */}
+            <div>Total Battles: {player.wins + player.losses}</div> {/* display total battles */}
+            <div>Total Earned: {player.earnings - player.costs} DC</div> {/* display total earned */}
+          </div>
+        </li>
+      ))}
+    </ol>
+    </>
   );
 }
+
 
 export default App;
