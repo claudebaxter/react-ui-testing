a6a8113a1e49e0d2546f39583433c623dab25143
elborracho420 <e.baxter@dark-coin.io>
2023-04-04 20:24:51 -0500

    Starting form project for Dark Coin V2 Distribution Claims.

--- src/App.js
+++ src/App.js
@@ -1,158 +1,48 @@
-import gold from './gold.svg';
-import silver from './silver.svg';
-import bronze from './bronze.svg'
-
-const players = [
-  {
-    characterImg: 'https://ipfs.io/ipfs/Qmdy9r3M1EkhBNahzeHZmeLDtVRHTuK3k6hi5FFJiPEjuv',
-    characterName: 'Meatball Ron',
-    wins: 13,
-    losses: 4,
-    earnings: 100000,
-    costs: 10000
-  },
-  {
-    characterImg: 'https://ipfs.io/ipfs/QmbM1c4vbBFAG5dpsP21Eawcjm51ZXM3FiKEULVJsUNtuz',
-    characterName: 'Skully',
-    wins: 18,
-    losses: 6,
-    earnings: 130000,
-    costs: 10000
-  },
-  {
-    characterImg: 'https://ipfs.io/ipfs/QmXmYHhjM4aVxpjC37UF5oy1C9STXthu8g6AsT4rZX7iTD',
-    characterName: 'Odin',
-    wins: 15,
-    losses: 6,
-    earnings: 105000,
-    costs: 15000
-  },
-  {
-    characterImg: 'https://ipfs.io/ipfs/QmejW5UYbMMtV64y9faC9bwqhNSvGxULbeBMGuoeZcjMgj',
-    characterName: 'Evil Pooh',
-    wins: 9,
-    losses: 3,
-    earnings: 75000,
-    costs: 15000
-  }
-];
-
-
 function App() {
-  // sort the players by wins, then by losses, then by earnings
-  const sortedPlayers = players.sort((a, b) => {
-    if (a.wins !== b.wins) {
-      return b.wins - a.wins; // sort by wins descending
-    } else if (a.losses !== b.losses) {
-      return a.losses - b.losses; // sort by losses ascending
-    } else {
-      return b.earnings - a.earnings; // sort by earnings descending
-    }
-  });
 
   return (
     <>
-    <h1 style={{
-        display: 'flex',
-        margin: 'auto',
-        justifyContent: 'center',
-        color: 'white',
-        marginTop: '30px',
-        whiteSpace: 'nowrap',
-        minWidth: '732px'
-    }}>
-        Dark Coin Arena Leaderboard
-    </h1>
-    <ol
-      style={{
-        backgroundColor: 'black',
-        color: 'white',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        border: '5px solid white',
-        borderRadius: '15px',
-        marginLeft: '15%',
-        marginRight: '15%',
-        marginTop: '5%',
-        padding: '20px',
-        flexDirection: 'column',
-        paddingLeft: '20px',
-        minWidth: '550px'
-      }}
-    >
-      <h1 style={{
-        display: 'flex',
-        margin: 'auto',
-        justifyContent: 'center',
-        marginBottom: '20px'
-    }}>
-        BATTLE RANK
-      </h1>
-
-      {sortedPlayers.map((player, index) => (
-        <li 
-          key={index}
-          style={{
-            border: index % 2 === 0 ? '2px solid white' : 'none',
-            borderRadius: '15px',
-            marginBottom: '10px',
+      <div style={{
+        display: 'flex', 
+        justifyContent: 'center'
+      }}>
+          <div style={{
+            border: '5px solid rgb(255,255,255)', 
+            boxShadow: '0 0 15px 5px rgba(211,211,211,.75)', 
+            margin: '100px 100px', 
+            borderRadius: '20px', 
+            textAlign: 'center',
+            color: 'white',
             padding: '20px',
-            display: 'flex',
-            alignItems: 'center',
-            maxWidth: '90%',
-            position: 'relative'
-          }}
-        >
-          {index === 0 && (
-          <img 
-            src={gold} 
-            alt="gold" 
-            style={{
-              position: 'absolute',
-              top: '-10px',
-              left: 'calc(50% - 118px)'
-            }} 
-          />
-        )} {/* display gold crown icon above the first player */}
-        {index === 1 && (
-          <img 
-            src={silver} 
-            alt="silver" 
-            style={{
-              position: 'absolute',
-              top: '-10px',
-              left: 'calc(50% - 118px)'
-            }} 
-        />
-        )} {/* display silver crown icon above the second player */}
-        {index === 2 && (
-          <img 
-            src={bronze} 
-            alt="bronze" 
-            style={{
-              position: 'absolute',
-              top: '15px',
-              left: 'calc(50% - 118px)'
-            }} 
-          />
-        )} {/* display bronze crown icon above the third player */}
-          <div style={{ marginRight: '20px', fontSize: "35px" }}>{index + 1}{index === 0 ? 'st' : index === 1 ? 'nd' : index === 2 ? 'rd' : 'th'}</div> {/* display rank */}
-          <img src={player.characterImg} alt="character" 
-            style={{ 
-              height: '200px', 
-              flex: '0 0 200px', 
-              marginRight: '20px'
-            }} /> {/* display character image */}
-          <div style={{ flex: '1', fontSize: '30px' }}>
-            <div>Name: {player.characterName}</div> {/* display character name */}
-            <div>Wins: {player.wins}</div> {/* display wins */}
-            <div>Total Battles: {player.wins + player.losses}</div> {/* display total battles */}
-            <div>Total Earned: {player.earnings - player.costs} DC</div> {/* display total earned */}
+            maxWidth: '600px',
+            minWidth: '350px'}}>
+              <img src={process.env.PUBLIC_URL + '/icon.png'} 
+                alt="logo"
+                style={{
+                  alignItems: 'center',
+                  width: '85px',
+                  height: '85px',
+                  margin: '0, -35px'
+                }} /> 
+              <h1 style={{
+                fontFamily: 'Arial, sans-serif',
+                textAlign: 'center',
+                fontSize: '1.5em',
+                textTransform: 'uppercase',
+                margin: '0, auto, 10px, auto'
+              }}>Dark Coin V2 Token Claim Form</h1>
+              <p style={{
+                
+              }}>
+                Please fill out the form below accurately to open up a claim for DCV2 Distribution. 
+
+                Please understand that as claims are handled on a case by case basis and require individual analysis, 
+                it may take some time to respond and fulfill your claim--but we will ensure it is resolved as soon as possible.
+
+                This will be an ongoing effort until all of the users affected by the hack and following announcements are made whole.
+              </p>
           </div>
-        </li>
-      ))}
-    </ol>
+      </div>
     </>
   );
 }
